516a803bf6f22df191c890d121218d9bed9bd124
Trys10Studios <trys10studios@gmail.com>
2022-06-23 17:58:15 -0400

    branching to work on javascript buttons

---
+++ calculator.js
@@ -0,0 +1,6 @@
+const output = document.getElementById('output'); // Output for calculation
+const btn1 = document.getElementById('btn1'); // Output for calculation
+
+btn1.addEventListener('click', function handleClick() {
+    output.textContent = '1';
+});
