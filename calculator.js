7d42319e17adba6bf527ab4a48081f27f9291a76
Trys10Studios <trys10studios@gmail.com>
2022-06-23 18:37:25 -0400

    have to make sure the javascript file is at the end of the html file to work properly.

--- calculator.js
+++ calculator.js
@@ -2,5 +2,5 @@ const output = document.getElementById('output'); // Output for calculation
 const btn1 = document.getElementById('btn1'); // Output for calculation
 
 btn1.addEventListener('click', function handleClick() {
-    output.textContent = '1';
+    output.textContent = 1;
 });
